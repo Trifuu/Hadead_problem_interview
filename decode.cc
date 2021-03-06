795512f00b996d735d1b3d31dccc92310ba009d2
Trifu Marius-Constantin <trifumarius01@gmail.com>
2021-03-06 23:08:08 +0200

    update stilizare

--- decode.cc
+++ decode.cc
@@ -13,23 +13,21 @@ void decode(const std::string &symbol_table, std::string &decode_message) {
     
     int size = sizeof(size_t);
     size_t length = 0;
-    for( int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         std::cin.get(character);
-        length = character << 8*i & (0xff << 8*i) | length;
+        length = character << 8 * i & (0xff << 8 * i) | length;
     }
 
     decode_message = std::string(length, ' ');
-    
-    //decode the message
+    // decode the message
     for (int i = 0; i < decode_message.length() - 1; i += 2) {
 
         std::cin.get(character);
         decode_message[i] = symbol_table[(character >> 4) & 0xf];
         decode_message[i + 1] = symbol_table[character & 0xf];
     }
-    //if the message have an odd length decode the last character separately
-    if ( decode_message.length() % 2 == 1){
+    // if the message have an odd length decode the last character separately
+    if (decode_message.length() % 2 == 1) {
         std::cin.get(character);
         decode_message[decode_message.length() - 1] = symbol_table[character & 0xf];
     }
@@ -38,13 +36,13 @@ void decode(const std::string &symbol_table, std::string &decode_message) {
 int main() {
     std::string symbol_table, decode_message;
     
-    //read the unique symbol table
+    // read the unique symbol table
     read_symbol_table(symbol_table);
     
-    //decode the message using the symbol table
+    // decode the message using the symbol table
     decode(symbol_table, decode_message);
     
-    //print the decoded message to stdout
+    // print the decoded message to stdout
     std::cout.write(decode_message.c_str(), decode_message.length());
     
     return 0;
--- encode.cc
+++ encode.cc
@@ -3,12 +3,9 @@
 #define MAXIMUM_UNIQUE_SYMBOLS 16
 
 void read_all_symbols(std::string &input) {
-
     while(true) {
         input.push_back(std::cin.get());
-        
-        if (std::cin.eof())
-        {
+        if (std::cin.eof()) {
             input.pop_back();
             break;
         }   
@@ -16,44 +13,42 @@ void read_all_symbols(std::string &input) {
 }
 
 void create_symbol_table(const std::string &input, std::string &symbol_table) {
+    int input_length = input.length();
+    int symbol_table_length = input.length();
+    int i, j;
 
-    for (int i = 0; i < input.length(); i++) {
-        int j;
-
-        for (j = 0; j < symbol_table.length(); j++) {
+    for (i = 0; i < input_length; i++) {
+        for (j = 0; j < symbol_table_length; j++) {
             if (symbol_table[j] == input[i]) {
                 break;
             }
         }
-
-        if (j == symbol_table.length()) {
+        if (j == symbol_table_length) {
             //stop the search when it exceed the maximum of unique symbols
-            if(symbol_table.length() == MAXIMUM_UNIQUE_SYMBOLS)
-            {
+            if(symbol_table_length == MAXIMUM_UNIQUE_SYMBOLS) {
                 std:: cerr << "Error: The number of unique symbols is > 16." << std::endl;
                 exit(1);
             }
-
             symbol_table.push_back(input[i]);
         }
     }
 }
 
 int search_symbol_index(const char &input, const std::string &symbol_table) {
-    int index;
-    for (index = 0; index < symbol_table.length(); index++) {
-            if (symbol_table[index] == input) {
-                return index;
+    int i;
+
+    for (i = 0; i < symbol_table.length(); i++) {
+            if (symbol_table[i] == input) {
+                return i;
             }
         }
-    return index;
+    return i;
 }
 
 void encode(const std::string &input, const std::string &symbol_table,
             std::string &out) {
-
     char encoded_character;
-    
+
     //create the output encoded array
     for (int j = 0; j < input.length() - 1; j += 2) {
         int k1, k2;
@@ -77,8 +72,7 @@ void encode(const std::string &input, const std::string &symbol_table,
 }
 
 void write_to_stdout(const std::string &symbol_table, const std::string &out,
-                     std::string input)
-{
+                     std::string input) {
     //write to stdout the informations regarding the number and values for the symbol_table
     std::cout.put(symbol_table.length());
     std::cout.write(symbol_table.c_str(), symbol_table.length());
@@ -86,12 +80,12 @@ void write_to_stdout(const std::string &symbol_table, const std::string &out,
     //write to stdout the length of the input array of symbols
     int size = sizeof(size_t);
     size_t length = input.length();
-    for( int i = 0; i< size; i++)
-    {
+
+    for (int i = 0; i < size; i++) {
         std::cout.put((char)(length >> 8*i) & 0xff);
     }
-    
-    //write encoded array of symbols to stdout
+
+    // write encoded array of symbols to stdout
     std::cout.write(out.c_str(), (input.length() + 1) / 2);
 }
 
@@ -99,16 +93,16 @@ int main() {
 
     std::string input, out, symbol_table;
 
-    //read all the symbols from the input stdin
+    // read all the symbols from the input stdin
     read_all_symbols(input);
 
-    //create the symbol_table array
+    // create the symbol_table array
     create_symbol_table(input, symbol_table);
 
-    //calculate the output encoded array
+    // calculate the output encoded array
     encode(input, symbol_table, out);
 
-    //write to stdout all the encoded message informations
+    // write to stdout all the encoded message informations
     write_to_stdout(symbol_table, out, input);
 
     return 0;
