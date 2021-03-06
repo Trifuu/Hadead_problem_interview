8554b19b8e9b1fd040591412cb3b45742e375e5d
Trifu Marius-Constantin <trifumarius01@gmail.com>
2021-03-06 22:37:26 +0200

    terminat si readme

---
+++ README.md
@@ -0,0 +1,45 @@
+The program encode and decode an input file with symbols.
+
+The encription works by replacing the symbols with an index 
+from an unique symbols table that is created based on the input file.
+
+The limitation are:
+- no more then 16 unique symbols in the input file
+- no input file with more total symbols than the maximum limit of size_t which is very very big
+
+Don't load in the decode program a file that was not created with the encode one.
+
+You can compile and test the program using the test.sh script or you can compile it on your own.
+
+To compile:
+g++ encode.cc -o encode
+g++ decode.cc -o decode
+
+To run:
+./encode <example.dat >out.bin
+
+./decode <out.bin >out.txt
+
+To check if the encode -> decode works you can call the diff command:
+
+diff out.txt example.dat
+
+If the diff print something, there is a problem with the encription.
+
+To check if the encription works with different files with bigger vales and different number of symbols,
+you can use the script.sh file.
+
+To run the script.sh you just need to call it:
+
+./script.sh
+
+The file will create it's own tests and at the end will delete all the files that were created and keeps only
+log.txt if there were errors in one of the tests.
+
+In the top of the file you can find 2 arrays: number_of_total_symbols and number_of_unique_symbols
+
+You can change their values to customize your testing by following:
+- number_of_total_symbols -> you can specify the size of the file(total number of symbols)
+- number_of_unique_symbols -> you can specify the numbers of unique symbols
+
+The script will run all the combinations between number_of_total_symbols and number_of_unique_symbols
--- script.sh
+++ script.sh
@@ -4,31 +4,44 @@ rm -f log.txt
 touch log.txt
 mkdir tests
 
-number_of_total_symbols=(100 101)
+#number of symbols that will exist in the input file
+number_of_total_symbols=(1000 10001)
+
+#number of unique symbols that will exist in the input file that will have "number_of_total_symbols" symbols
 number_of_unique_symbols=(1 5 16)
 
 letters=({A..Z})
 
 for total_symbols in ${number_of_total_symbols[@]}; do
 	for number in  ${number_of_unique_symbols[@]}; do
-		number=$((number - 1))
-		symbol=${letters[${number}]}
-		dd if=<(tr -dc A-${symbol} </dev/urandom) bs=1 count=${total_symbols} of=tests/total_symbols_${total_symbols}_unique_symbols_${number}.in
+		echo "Enter in test with "${total_symbols}" total symbols and "${number}" of unique symbols."
+		number2=$((number - 1))
+		symbol=${letters[${number2}]}
+		dd if=<(tr -dc A-${symbol} </dev/urandom) bs=1 count=${total_symbols} of=tests/total_symbols_${total_symbols}_unique_symbols_${number}.in 2>/dev/null
 
 		./test.sh tests/total_symbols_${total_symbols}_unique_symbols_${number}.in > /dev/null
 		diff out.txt tests/total_symbols_${total_symbols}_unique_symbols_${number}.in > diff.txt
 		if [ -s diff.txt ]
 		then
-			echo "Failed: test with "${total_symbols}" total symbols and "$((number + 1))" unique symbols from A-"${symbol}"."
-			echo "Failed: test with "${total_symbols}" total symbols and "$((number + 1))" unique symbols from A-"${symbol}"." >> log.txt
+			echo "Failed: test with "${total_symbols}" total symbols and "${number}" unique symbols from A-"${symbol}"."
+			echo "Failed: test with "${total_symbols}" total symbols and "${number}" unique symbols from A-"${symbol}"." >> log.txt
 			echo -e "diff out.txt tests/total_symbols_${total_symbols}_unique_symbols_${number}.in > diff.txt\n" >> log.txt
-			#cat diff.txt >> log.txt
-			echo -e "-------------------------------------------------------\n\n" >> log.txt
-		fi
 
+			echo -e "-------------------------------------------------------\n\n" >> log.txt
+		else
+			echo "Test passed. OK"
+		fi
+		echo ""
 		rm -f diff.txt
 	done
 
 done
+
+if [ ! -s log.txt ]; then
+	echo "All tests passed. OK"
+	rm log.txt
+fi
+
+#remove all the files that were created in this script
 rm -rf tests
 rm out.txt out.bin
